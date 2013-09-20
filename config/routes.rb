160032773e261bf9aab5a805d352abdbbd4f7595
Patrick Lee <patrick.robin.lee@gmail.com>
2013-09-19 19:28:38 -0700

    Enhanced tests for static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/about"
-  get "static_pages/contact"
-  get "static_pages/home"
-  get "static_pages/help"
+  root 'static_pages#home'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
