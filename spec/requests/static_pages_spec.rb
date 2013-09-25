bd2f24730c546dfa15b09919f5c5556d040b927d
Patrick Lee <patrick.robin.lee@gmail.com>
2013-09-24 23:52:35 -0700

    Finished chapter 5.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,22 +5,32 @@ describe "StaticPages" do
   let(:web_site_title){"Ruby on Rails Tutorial Sample App"}
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it {should have_selector('h1', text: heading) }
+    it {should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
-    before {visit root_path}
-    it {should have_content('Sample App')}
-    it {should have_title(full_title('')) }
+    before { visit root_path}
+    let(:heading)         { 'Sample App' }
+    let(:page_title)      { '' }
+
+    it_should_behave_like "all static pages"
     it {should_not have_title('| Home)') }
   end
 
   describe "Help page" do
-    before{visit help_path}
-    it {should have_content('Help')}
-    it {should have_title(full_title('Help')) }
+    before { visit help_path}
+    let(:heading)         { 'Help' }
+    let(:page_title)      { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before{visit about_path}
     it {should have_content('About')}
+    it {should have_selector('h1', text:'About')}
     it {should have_title(full_title('About')) }
   end
 
@@ -28,5 +38,17 @@ describe "StaticPages" do
     before{visit contact_path}
     it {should have_selector('h1', text:'Contact')}
     it {should have_title(full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    expect(page).to have_title('Sample App')
   end
 end
